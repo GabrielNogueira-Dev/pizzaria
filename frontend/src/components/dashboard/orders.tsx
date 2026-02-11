9abd164b4be6c40f6d36939ae69e07946552be02
Gabriel Nogueira <gabrielfariass-99@hotmail.com>
2026-02-11 12:59:00 +0000

    detalhes da order sendo executados

--- frontend/src/components/dashboard/orders.tsx
+++ frontend/src/components/dashboard/orders.tsx
@@ -1,10 +1,13 @@
 "use client"
 
-import { RefreshCcw } from "lucide-react"
+import { EyeIcon, RefreshCcw } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { useEffect, useState } from "react";
 import { Order } from "@/lib/types";
 import { apiClient } from "@/lib/api";
+import { Badge } from "../ui/badge";
+import { Card,CardTitle,CardContent, CardHeader } from "../ui/card";
+import { OrderModal } from "./order-modal";
 
 interface OrdersProps{
 token:string
@@ -14,6 +17,7 @@ export function Orders({token} :OrdersProps){
     
     const [loading,setLoading] = useState(true);
     const [orders,setOrders] = useState<Order[]>([])
+    const [selectedOrder,setSelectedOrder] = useState<null | string>(null)
 
     const fetchOrders = async () => {
         
@@ -23,7 +27,9 @@ export function Orders({token} :OrdersProps){
                 cache:"no-store",
                 token:token
             })
-            setOrders(response)
+                const pendingOrders = response.filter(order=> !order.status)
+
+            setOrders(pendingOrders)
             setLoading(false)
         }catch(error){
            setLoading(false)
@@ -38,6 +44,15 @@ export function Orders({token} :OrdersProps){
        loadOrders()
     },[])
 
+    /*const calculateOrderTotal = (order:Order) => {
+        return order.items?.reduce((total,item)=> {
+            return total + item.product.price * item.amount / 100
+        },0).toLocaleString("pt-BR",{
+            style:"currency",
+            currency:"BRL"
+        })
+    } */
+
     return(
          <div className="space-y-4 sm:space-y-6">
                <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-4">
@@ -46,10 +61,77 @@ export function Orders({token} :OrdersProps){
                    <p className="text-sm sm:text-base mt-1">Organize suas categorias</p>
                  </div>
        
-                 <Button className="bg-brand-primary text-white hover:bg-brand-primary">
+                 <Button className="bg-brand-primary text-white hover:bg-brand-primary"
+                 onClick={fetchOrders}>
                     <RefreshCcw/>
                  </Button>
                </div> 
-               </div>
+
+        {loading? (
+            <div className="text-center text-gray-300">
+                <p>Carregando pedidos..</p>
+            </div >
+        ): orders.length === 0 ? (
+            <div>
+                <p className="text-center text-gray-300">Pedido não encontrado!</p>
+            </div>
+        ): (
+            <div className=" grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                {orders.map((order) => (
+                  <Card key={order.id} className="bg-app-card border-app-border text-white">
+                    <CardHeader>
+                      <div className="flex items-center justify-between gap-2">
+                        <CardTitle className="text-lg lg:text-xl font-bold">
+                           Mesa {order.table}
+                        </CardTitle>
+                        <Badge variant="secondary" className=" text-xs select-none">
+                          produção
+                        </Badge>
+                      </div>
+                    </CardHeader>
+
+                    <CardContent>
+                        <div>
+                            {order.items && order.items.length > 0 && (
+                                <div className="space-y-1">
+                                    {order.items.slice(0,2).map((item)=> (
+                                        <p className="text-xs sm:text-sm text-gray-300 truncate" key={item.id}>
+                                            {item.amount}x - {item.product.name}
+                                        </p>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+
+                        <div className="flex flex-col xl:flex-row justify-between pt-4 border-t border-app-border gap-3">
+                            <div className="self-start">
+                              <p className="text-sm md:text-base text-gray-400">Total</p>
+                              <p className="text-base font-bold text-brand-primary">{order.items?.reduce((total,item)=> 
+                                total + item.product.price * item.amount / 100,0).toLocaleString("pt-BR",{
+                                    style:"currency",
+                                    currency:"BRL"
+                                })
+                                }</p>
+                            </div>
+                            <Button size="sm" className="bg-brand-primary hover:bg-brand-primary"
+                            onClick={()=> setSelectedOrder(order.id)} >
+                              <EyeIcon className="w-5 h-5"/> Detalhes
+                            </Button>
+                        </div>
+                    </CardContent>
+
+                  </Card>
+                ))}
+            </div>
+        )}
+        <OrderModal
+          orderId= {selectedOrder}
+          onClose = {async ()=> {
+            setSelectedOrder(null);
+            await fetchOrders();
+          }}
+          token={token}
+        />
+          </div>
     )
 }
