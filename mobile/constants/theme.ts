ca105973a9e1262974f4c11f43d9a24ea6c4d44e
Gabriel Nogueira <gabrielfariass-99@hotmail.com>
2026-02-18 09:10:21 +0000

    Adicionando projeto mobile

---
+++ mobile/constants/theme.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  background: "#1D1D2E",
+  backgroundInput: "#101026",
+  primary: "#FFFFFF",
+  brand: "#FF3F48",
+  green: "#3FFFA3",
+  red: "#FF3F4B",
+  gray: "#BABABA",
+  borderColor: "#2E2E3E",
+};
+
+export const spacing = {
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 24,
+};
+
+export const borderRadius = {
+  sm: 12,
+  md: 14,
+  lg: 16,
+  xl: 20,
+  xxl: 24,
+};
